fa185c95fb260cf22d95fd93b504a72317ae8b55
Adrian Storr <Adrianloves123@gmail.com>
2020-11-04 22:23:25 -0500

    created apiroute

---
+++ routes/apiRoutes.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// db.json file path
+const DB_PATH = path.join(__dirname, "../db/db.json");
+
+module.exports = function (app) {
+
+    // response with the list of notes
+    app.get('/api/notes', async (req, res) => {
+
+        let notes = await readFileAsync(DB_PATH, "utf-8");
+
+        if (notes) {
+            notes = JSON.pa
