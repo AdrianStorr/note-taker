0a0b40b6270c00e8deea9cbb80d8f9b547975eda
Adrian Storr <Adrianloves123@gmail.com>
2020-11-04 22:19:23 -0500

    created html routes for notes and index

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+
+const path = require('path');
+
+module.exports = function (app) {
+
+    // request to notes.html
+    app.get('/notes', (req,res) => {
+        res.sendFile(path.join(__dirname,'../public/notes.html'));
+    });
+
+    
+    // default url
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname,'../public/index.html'));
+    });
+    
+};
