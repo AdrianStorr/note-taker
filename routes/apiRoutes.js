75f9d4dbe6fae110418963717d368b75827b69d4
Adrian Storr <Adrianloves123@gmail.com>
2020-11-04 22:24:57 -0500

    added the crud feature

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,55 @@
 const path = require('path');
 const fs = require('fs');
 const util = require("util");
-
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
-
 // db.json file path
 const DB_PATH = path.join(__dirname, "../db/db.json");
-
 module.exports = function (app) {
-
     // response with the list of notes
     app.get('/api/notes', async (req, res) => {
-
         let notes = await readFileAsync(DB_PATH, "utf-8");
-
         if (notes) {
-            notes = JSON.pa
+            notes = JSON.parse(notes);
+            res.json(notes);
+        } else {
+            res.send(false);
+        }
+        res.end();
+    });
+    // adds new notes
+    app.post('/api/notes', async (req, res) => {
+        const { title, text } = req.body;
+        const data = {
+            title: title,
+            text: text,
+            id: Date.now()
+        };
+        let notes = await readFileAsync(DB_PATH, "utf-8");
+        if (notes) {
+            notes = JSON.parse(notes);
+            notes.push(data);
+        } else {
+            notes = [data];
+        }
+        await writeFileAsync(DB_PATH, JSON.stringify(notes), "utf-8");
+        res.json(notes);
+        res.end()
+    })
+    // deletes notes
+    app.delete('/api/notes/:id', async (req, res) => {
+        let { id } = req.params;
+        id = parseInt(id);
+        let notes = await readFileAsync(DB_PATH, "utf-8");
+        if (notes) {
+            notes = JSON.parse(notes);
+            notes = notes.filter(notes => notes.id !== id);
+            await writeFileAsync(DB_PATH, JSON.stringify(notes), "utf8");
+            res.json(notes);
+            res.end();
+        } else {
+            res.send(false);
+            res.end();
+        }
+    });
+}
