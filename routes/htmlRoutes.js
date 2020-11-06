951126777f4d88e5c83ae085230d1fbf8e951f76
Adrian Storr <Adrianloves123@gmail.com>
2020-11-05 20:12:40 -0500

    updated html.Routes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,9 @@
 const path = require('path');
 
 module.exports = function (app) {
-
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname,'../public/index.html'));
+    });
     // request to notes.html
     app.get('/notes', (req,res) => {
         res.sendFile(path.join(__dirname,'../public/notes.html'));
